4c38ccdc92c6cece1640d360bd5444921ce33a26
Stanimir Penkov <70176653+Penk0vXd@users.noreply.github.com>
2026-06-16 13:17:00 +0300

    Add files via upload

--- Izpit06/Izpit06/Node.cs
+++ Izpit06/Izpit06/Node.cs
@@ -1,54 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Izpit06
 {
     internal class Node
     {
-        class Node
-        {
-            public int Value { get; set; }
-            public Node(int value)
-            {
-                Value = value;
-            }
-        }
-        class DynamicNodeArray <T>
-        {
-            private Node[] nodes;
-            private int count;
-            public DynamicNodeArray()
-            {
-                nodes = new Node[2];
-                count = 0;
-            }
-            public void Add(int value) 
-            {
-                nodes[count++] = new Node(value);
-            }
-            private void Resize() 
-            {
-                
-            }
-            public int Count() 
-            {
-                return count;
-            }
-            public int GetValue(int index)
-            {
-                return nodes[index].Value;
-            }
-            public void SetValue(int index, int value) 
-            {
+        public int Value { get; set; }
 
-            }
-            public void Print() 
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    internal class DynamicNodeArray
+    {
+        private Node[] nodes;
+        private int count;
+
+        public DynamicNodeArray()
+        {
+            nodes = new Node[2];
+            count = 0;
+        }
+
+        public void Add(int value)
+        {
+            if (count == nodes.Length)
             {
-                
+                Resize();
             }
+
+            nodes[count] = new Node(value);
+            count++;
+        }
+
+        private void Resize()
+        {
+            Node[] newArray = new Node[nodes.Length * 2];
+
+            for (int i = 0; i < count; i++)
+            {
+                newArray[i] = nodes[i];
+            }
+
+            nodes = newArray;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public int GetValue(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return nodes[index].Value;
+        }
+
+        public void SetValue(int index, int value)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            nodes[index].Value = value;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(nodes[i].Value + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
--- Izpit06/Izpit06/QuickSort.cs
+++ Izpit06/Izpit06/QuickSort.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,45 @@ namespace Izpit06
 {
     internal class QuickSort
     {
-        
+        public void Sort(DynamicNodeArray array)
+        {
+            if (array == null || array.Count() < 2)
+            {
+                return;
+            }
+
+            QuickSortMethod(array, 0, array.Count() - 1);
+        }
+
+        private void QuickSortMethod(DynamicNodeArray array, int left, int right)
+        {
+            if (left >= right)
+            {
+                return;
+            }
+
+            int pivotIndex = Partition(array, left, right);
+
+            QuickSortMethod(array, left, pivotIndex - 1);
+            QuickSortMethod(array, pivotIndex + 1, right);
+        }
+
+        private int Partition(DynamicNodeArray array, int left, int right)
+        {
+            int pivot = array.GetValue(right);
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array.GetValue(j) <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, right);
+            return i + 1;
+        }
     }
 }
