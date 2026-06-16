9af9a72ee8d4a07bf71a24a59797e335604f7df8
Stanimir Penkov <70176653+Penk0vXd@users.noreply.github.com>
2026-06-16 12:55:53 +0300

    Add files via upload

---
+++ Izpit06/Izpit06/Node.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izpit06
+{
+    internal class Node
+    {
+        class Node
+        {
+            public int Value { get; set; }
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+        class DynamicNodeArray <T>
+        {
+            private Node[] nodes;
+            private int count;
+            public DynamicNodeArray()
+            {
+                nodes = new Node[2];
+                count = 0;
+            }
+            public void Add(int value) 
+            {
+                nodes[count++] = new Node(value);
+            }
+            private void Resize() 
+            {
+                
+            }
+            public int Count() 
+            {
+                return count;
+            }
+            public int GetValue(int index)
+            {
+                return nodes[index].Value;
+            }
+            public void SetValue(int index, int value) 
+            {
+
+            }
+            public void Print() 
+            {
+                
+            }
+        }
+    }
+}
---
+++ Izpit06/Izpit06/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Izpit06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 2, 9, 1, 7 };
+
+            int c = 0;
+            int middle = (array.Length - 1) / 2;
+            int pivot = array.Length - 1;
+
+            c = array[pivot];
+            array[pivot] = array[middle];
+            array[middle] = array[c];
+
+            bool goo = false;
+
+            for (int i = 0; i < array.Count(); i++)
+            {
+                for (int j = 0; j < array.Count(); j++)
+                {
+                    if (array[pivot] >= array[j])
+                    {
+                        
+                    }
+                }
+            }
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
